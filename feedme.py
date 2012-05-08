fcd801155a6bada8d83ec73881c6013dba02d613
= <tonyblundell@gmail.com>
2012-05-08 01:28:27 +0100

    Dupe bug

--- feedme.py
+++ feedme.py
@@ -20,10 +20,6 @@ def main():
         # Parse the feed's URL using the feedparser library
         parsed = feedparser.parse(feed['feed_url'])
 
-        # Grab the last item in the database for comparison
-        last = coll.find().sort('time', -1).limit(1)
-        last = last[0] if last.count() > 0 else None
-
         # Add each entry in turn
         for entry in parsed.entries:
 
@@ -34,11 +30,13 @@ def main():
             # Parse the entry's date/time to a datetime object
             entry.date_parsed = datetime.fromtimestamp(mktime(entry.date_parsed)) if entry.date_parsed else None
 
-            # Use the date/time as an ID if one isn't present (IDs are used to prevent inserting dupes to the DB)
+            # If the entry doesn't have an ID, use it's date/time instead
             entry.id = entry.id or entry.date_parsed
 
             # Filter any unwanted text from the entrie's title
             entry.title = entry.title.replace(feed['string_to_filter_from_posts'], '') if feed['string_to_filter_from_posts'] else entry.title
+            entry.title = entry.title.strip()
+            entry.title = entry.title if entry.title.startswith('http') else entry.title[0].upper() + entry.title[1:]
 
             # If we successfully got an ID, date/time and title from the feed, we can insert into the DB
             if entry.id and entry.date_parsed and entry.title:
@@ -53,18 +51,7 @@ def main():
                         'detail': entry.title,
                         'url': entry.link
                     }
-
-                    # If this entry has the same text as the last one, just update its timestamp (if necessary)
-                    if last and last['detail'] == entry.title:
-                        if last['time'] < entry.date_parsed:
-                            coll.update(last, doc)
-
-                    # If this entry is different to the last one, add a new record
-                    else:
-                        coll.insert(doc)
-
-                    # The new entry now becomes the last so we can check against it in the next iteration
-                    last = doc
+                    coll.insert(doc)
 
 
 if __name__ == '__main__':
