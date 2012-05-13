b4719cb8fe8d5524f9be452b5bbbf5a55389de41
tony <tony@dev.(none)>
2012-05-13 21:19:57 +0100

    Feed reader fix

--- feedme.py
+++ feedme.py
@@ -23,26 +23,27 @@ def main():
         # Add each entry in turn
         for entry in parsed.entries:
 
-            # Pull the required attributes from the entry object
-            for attr in ('id', 'date_parsed', 'title', 'link'):
-                setattr(entry, attr, getattr(entry, attr, None))
-
             # Parse the entry's date/time to a datetime object
-            entry.date_parsed = datetime.fromtimestamp(mktime(entry.date_parsed)) if entry.date_parsed else None
+            entry.date_parsed = getattr(entry, 'date_parsed', None)
+            entry.date_parsed = datetime.fromtimestamp(mktime(entry.date_parsed)) if entry.date_parsed else datetime.now()
 
             # If the entry doesn't have an ID, use it's date/time instead
-            entry.id = entry.id or entry.date_parsed
+            entry.id = getattr(entry, 'id', entry.date_parsed)
 
             # Filter any unwanted text from the entrie's title
+            entry.title = getattr(entry, 'title', '')
             entry.title = entry.title.replace(feed['string_to_filter_from_posts'], '') if feed['string_to_filter_from_posts'] else entry.title
             entry.title = entry.title.strip()
             entry.title = entry.title if entry.title.startswith('http') else entry.title[0].upper() + entry.title[1:]
+
+            # The entry may have a link, set to None if not, so we can still ref the attr
+            entry.link = getattr(entry, 'link', None)
 
             # If we successfully got an ID, date/time and title from the feed, we can insert into the DB
             if entry.id and entry.date_parsed and entry.title:
 
                 # Check for this feed's ID in the DB, don't insert twice
-                if not coll.find_one({'id': entry.id}):
+                if not coll.find_one({'id': entry.id}) and not coll.find_one({'detail': entry.title}):
 
                     # Create a doc for mongo (a python dict will do)
                     doc = {
