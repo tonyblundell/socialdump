e93598ffeba7d28d3559bea9d8b3f8fbe7475ed1
Tony Blundell <tonyblundell@gmail.com>
2012-09-13 00:48:58 +0100

    Mongoengine pt1

---
+++ models.py
@@ -0,0 +1,49 @@
+import datetime
+import feedparser
+from mongoengine import *
+
+
+class Post(EmbeddedDocument):
+    uid = StringField(required=True)
+    txt = StringField(required=True)
+    dt = DateTimeField(required=True)
+    url = URLField()
+
+    def __unicode__(self):
+        return self.txt.encode('utf-8')[:10]
+
+
+class Feed(Document):
+    lbl = StringField(required=True, primary_key=True)
+    furl = URLField(required=True) # Feed URL
+    surl = URLField() # Site URL
+    lnk = BooleanField(default=False) # Entire post is a link
+    strp = StringField(default='') # String to strip from start of all posts
+    psts = CappedSortedListField(EmbeddedDocumentField(Post), cap=5, ordering='dt', reverse=True)
+
+    def __unicode__(self):
+        return self.lbl.encode('utf-8')
+
+    def parse_feedparser_entry(self, entry):
+        uid = getattr(entry, 'id', None) or entry.link
+        for post in self.psts:
+            if post.uid == uid:
+                return
+        post = Post()
+        post.uid = uid
+        post.txt = entry.title.replace(self.strp, '').capitalize()
+        post.dt = datetime.datetime(*(entry.published_parsed[:6]))
+        post.url = entry.link
+        self.psts.append(post)
+
+    def pull(self):
+        for entry in feedparser.parse(self.furl).entries:
+            self.parse_feedparser_entry(entry)
+        self.save()
+        self.reload()
+
+
+if __name__ == '__main__':
+    connect('socialdump')
+    for feed in Feed.objects:
+        feed.pull()
