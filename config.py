441e78e340853b933664d8babd05b719d8eb53f0
Tony Blundell <tonyblundell@gmail.com>
2012-11-27 22:50:27 +0000

    Make prettier

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ MONGODB_DB = 'socialdump'
 MONGODB_PORT = 27017
 SOCIALDUMP_EMAIL = 'tony@tonyblundell.net'
 SOCIALDUMP_HEADING = 'Tony Blundell'
-SOCIALDUMP_SUBHEADING = 'Talented Programmer, Crap Guitarist. Sheffield UK.'
+SOCIALDUMP_SUBHEADING = 'Web Developer, Sheffield UK.'
