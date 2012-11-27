19aab15fe56345eeb74f06ff0fe2c79accc4cf32
Tony Blundell <tonyblundell@gmail.com>
2012-11-27 22:56:37 +0000

    Config - update email

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ DEBUG = True
 HOST = '0.0.0.0'
 MONGODB_DB = 'socialdump'
 MONGODB_PORT = 27017
-SOCIALDUMP_EMAIL = 'tony@tonyblundell.net'
+SOCIALDUMP_EMAIL = 'tonyblundell@gmail.com'
 SOCIALDUMP_HEADING = 'Tony Blundell'
 SOCIALDUMP_SUBHEADING = 'Web Developer, Sheffield UK.'
